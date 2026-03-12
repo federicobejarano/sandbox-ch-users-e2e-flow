dac12486a1929b70bbbcc0b2691e54fef6597c71
Federico Bejarano <federicojuanbejarano@gmail.com>
2026-03-12 10:31:44 -0300

    integrar framework Ionic y configurar herramientas del CLI en el frontend

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,10 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()), provideIonicAngular({})
   ]
 };
