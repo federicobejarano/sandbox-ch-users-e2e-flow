3f65e4932626985d797d8042fab75437a692443b
Federico Bejarano <federicojuanbejarano@gmail.com>
2026-03-13 11:13:19 -0300

    chore: dejar de rastrear el caché de Angular e ignorarlo

--- frontend/.angular/cache/21.2.2/users/vite/deps_ssr/chunk-2ARUJ5NV.js
+++
@@ -1,9 +0,0 @@
-import { createRequire } from 'module';const require = createRequire(import.meta.url);
-
-// node_modules/@ionic/core/components/p-C53feagD.js
-var o = (o2) => o2 && "" !== o2.dir ? "rtl" === o2.dir.toLowerCase() : "rtl" === (null === document || void 0 === document ? void 0 : document.dir.toLowerCase());
-
-export {
-  o
-};
-//# sourceMappingURL=chunk-2ARUJ5NV.js.map
--- frontend/.angular/cache/21.2.2/users/vite/deps_ssr/p-BmVRXR1y-OIPD7SXE.js
+++
@@ -1,8 +0,0 @@
-import { createRequire } from 'module';const require = createRequire(import.meta.url);
-import {
-  e
-} from "./chunk-SDFEJLT7.js";
-import "./chunk-6DU2HRTW.js";
-export {
-  e as startFocusVisible
-};
